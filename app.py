60b3593590a507951e3feb4dce3ec7d5d185c729
Zalán Fülöp <zalan12f@gmail.com>
2024-01-06 12:49:25 +0100

    Creating app.py header fiels

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///pizza.db")
