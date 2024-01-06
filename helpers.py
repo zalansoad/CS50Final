b62df41eca375f833601b8d5ebc881783833e54d
Zalán Fülöp <zalan12f@gmail.com>
2024-01-06 13:51:51 +0100

    update

--- helpers.py
+++ helpers.py
@@ -1,1 +1,13 @@
 from functools import wraps
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
